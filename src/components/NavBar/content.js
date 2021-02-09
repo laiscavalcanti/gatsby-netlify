df00386dab3917bcee62c9bc7d8ebc8a7ad1e411
Lais Cavalcanti <cavalcantislais@gmail.com>
2021-02-09 13:06:45 -0300

    alteraçoes no menu

--- src/components/NavBar/content.js
+++ src/components/NavBar/content.js
@@ -2,21 +2,21 @@ import React from "react"
 import Icon from "./Icon"
 
 const links = [
-  {
-    label: "Cursos",
-    url: "/tags/cursos/",
-    icon: "",
-  },
+  //{
+    //label: "Cursos",
+    //url: "/tags/cursos/",
+    //icon: "",
+  //},
   {
     label: "Subsidiário",
     url: "/tags/subsidiario/",
     icon: "",
   },
-  {
-    label: "Homeschooling",
-    url: "/tags/homeschooling/",
-    icon: "",
-  },
+  //{
+    //label: "Homeschooling",
+    //url: "/tags/homeschooling/",
+    //icon: "",
+  //},
   {
     label: "",
     url: "/search/",
